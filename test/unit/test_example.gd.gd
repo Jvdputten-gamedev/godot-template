bb84d9ae72a8b9c340d035b0db9e4ce6971c9e82
papydur <joostvdp92@gmail.com>
2024-05-09 08:25:16 +0200

    unit test example

--- test/unit/test_example.gd.gd
+++ test/unit/test_example.gd.gd
@@ -1,11 +1,28 @@
-extends Node
+extends GutTest
 
+func before_each():
+	gut.p("ran setup", 2)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func after_each():
+	gut.p("ran teardown", 2)
 
+func before_all():
+	gut.p("ran run setup", 2)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func after_all():
+	gut.p("ran run teardown", 2)
+
+func test_assert_eq_number_not_equal():
+	assert_eq(1, 2, "Should fail.  1 != 2")
+
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
+
+func test_assert_true_with_true():
+	assert_true(true, "Should pass, true is true")
+
+func test_assert_true_with_false():
+	assert_true(false, "Should fail")
+
+func test_something_else():
+	assert_true(false, "didn't work")
