e21090763a40c01800ef43637e6199585ac69267
Joost <jvdputten@thetavision.nl>
2024-05-10 01:07:16 +0200

    Move default assets

--- core/options_menu/options_menu.gd
+++ core/options_menu/options_menu.gd
@@ -2,11 +2,13 @@ class_name OptionsMenu
 extends Control
 
 @export var back_button: Button
+@export var settings_tab_container: SettingsTabContainer
 
 signal exit_options_menu
 
 func _ready():
 	back_button.button_down.connect(_on_back_button_down)
+	settings_tab_container.exit_options_menu.connect(_on_back_button_down)
 	set_process(false)
 
 func enable():
