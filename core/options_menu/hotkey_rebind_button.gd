b0912521b1c1636de870bbc2bf80a837670cf274
Joost <jvdputten@thetavision.nl>
2024-05-09 21:48:50 +0200

    rebind actions in settings

--- core/options_menu/hotkey_rebind_button.gd
+++ core/options_menu/hotkey_rebind_button.gd
@@ -7,18 +7,22 @@ extends Control
 
 func _ready():
 	set_process_unhandled_key_input(false)
+	set_action_name()
+	set_text_for_key()
 	button.toggled.connect(_on_button_toggled)
 
 func set_action_name() -> void:
 	label.text = "Unassigned"
 
+	# add all action names here and a name that will appear on the button>>>.
+	print(action_name)
 	match action_name:
 		"move_left":
 			label.text = "Move Left"
 		"move_right": 
 			label.text = "Move Right"
 		"jump":
-			label.jump = "Jump"
+			label.text = "Jump"
 
 
 func set_text_for_key() -> void:
@@ -29,9 +33,33 @@ func set_text_for_key() -> void:
 
 	button.text = "%s" % action_keycode
 
-func _on_button_toggled(toggled: bool) -> void:
-	if toggled:
+func _on_button_toggled(button_pressed: bool) -> void:
+	if button_pressed:
 		button.text = "Press any key..."
-		set_process_unhandled_key_input(toggled)
+		set_process_unhandled_key_input(button_pressed)
+
+		for i in get_tree().get_nodes_in_group("hotkey_button"):
+			if i.action_name != self.action_name:
+				i.button.toggle_mode = false
+				i.set_process_unhandled_key_input(false)
 	else:
+
+		for i in get_tree().get_nodes_in_group("hotkey_button"):
+			if i.action_name != self.action_name:
+				i.button.toggle_mode = true
+				i.set_process_unhandled_key_input(false)
 		set_text_for_key()
+
+
+func _unhandled_key_input(event) -> void:
+	rebind_action_key(event)
+	button.button_pressed = false
+
+func rebind_action_key(event) -> void:
+	InputMap.action_erase_events(action_name)
+	InputMap.action_add_event(action_name, event)
+
+	set_process_unhandled_input(false)
+	set_text_for_key()
+	set_action_name()
+
